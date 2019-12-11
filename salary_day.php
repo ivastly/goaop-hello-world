b783816a88369ea4cda0ed8251c061f98da6bc81
Vasiliy <catanfa@yandex.ru>
2019-12-11 21:34:14 +0100

    working

--- salary_day.php
+++ salary_day.php
@@ -1,13 +1,12 @@
 <?php declare(strict_types=1);
 
-use Ivastly\GoAopHelloWorld\Aop\ApplicationAspectKernel;
 use Ivastly\GoAopHelloWorld\BankingSystem\Bank;
 use League\CLImate\CLImate;
 
 require_once 'vendor/autoload.php';
 
 /* Initialize AOP engine */
-$applicationAspectKernel = ApplicationAspectKernel::getInstance();
+/*$applicationAspectKernel = ApplicationAspectKernel::getInstance();
 $applicationAspectKernel->init(
 	[
 		'appDir'       => __DIR__ . '/..',
@@ -16,7 +15,7 @@ $applicationAspectKernel->init(
 			__DIR__ . '/src',
 		],
 	]
-);
+);*/
 /* --- */
 
 $climate = new CLImate();
@@ -25,7 +24,7 @@ $bank    = new Bank($climate);
 $climate->backgroundLightMagenta(
 	"Dear everyone!
 Now is the salary day. 
-We are going to withdraw money from our company's bank account and send the money to our personal accounts!"
+We are going to withdraw money from our company's bank account and send the money to employees' accounts!"
 );
 
 $bank->withdrawMoney(1000, 'Docler Holding');
